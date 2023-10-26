d1bdaba62684390134ed064b1eb52960ad1a77e6
Sundeep D H <sundeep.dh@boeing.com>
2023-10-26 23:13:15 +0530

    sixth commit

---
+++ src/app/modules/admin/components/personal-space-home/personal-space-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-space-home',
+  templateUrl: './personal-space-home.component.html',
+  styleUrls: ['./personal-space-home.component.scss']
+})
+export class PersonalSpaceHomeComponent {
+
+}
